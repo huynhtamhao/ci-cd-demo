74b8bf3b2b8effdeeea2047cb6fecce335b0de48
Hao Huynh <huynhtamhao@gmail.com>
2023-04-25 14:32:54 +0700

    test 1

--- src/main/java/com/amaris/demo/DemoApplication.java
+++ src/main/java/com/amaris/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.amaris.demo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println("Current Time: ============== " + LocalDateTime.now() + "============== ");
         SpringApplication.run(DemoApplication.class, args);
     }
 
