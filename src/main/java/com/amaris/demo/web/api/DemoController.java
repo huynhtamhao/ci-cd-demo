f69377c1cf6cceda6db28aa2b51f4043bf656476
Hao Huynh <huynhtamhao@gmail.com>
2023-04-26 23:55:18 +0700

    add api

--- src/main/java/com/amaris/demo/web/api/DemoController.java
+++ src/main/java/com/amaris/demo/web/api/DemoController.java
@@ -1,5 +1,6 @@
 package com.amaris.demo.web.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/demo")
 public class DemoController {
 
+    @Value("${amaris.demo}")
+    String demoText;
+
     @GetMapping("")
     public String demo() {
-        return "demo";
+        return "Demo Text" + demoText;
     }
 }
